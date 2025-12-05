59babb2a0cb3ad17160a798a6b4fc3c9e25fa7dc
joseguadalupesotobecerra9605 <jsoto@siler-mx.com>
2025-12-05 11:53:03 -0600

    fix(RHAPPEMP1764784515787 Token para identificar empleado en app): RHAPPEMP1764784515787 Token para identificar empleado en app
    se modifico la migracion de la tabla employee devices para mayor tamaño

--- database/migrations/1764946474026_create_employee_devices_table.ts
+++ database/migrations/1764946474026_create_employee_devices_table.ts
@@ -7,10 +7,10 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('employee_device_id')
       table.string('employee_device_token', 250).notNullable().unique()
-      table.string('employee_device_model', 50).notNullable()
-      table.string('employee_device_brand', 50).notNullable()
-      table.string('employee_device_type', 50).notNullable()
-      table.string('employee_device_os', 50).notNullable()
+      table.string('employee_device_model', 250).notNullable()
+      table.string('employee_device_brand', 250).notNullable()
+      table.string('employee_device_type', 250).notNullable()
+      table.text('employee_device_os').notNullable()
       table.tinyint('employee_device_active').defaultTo(1).notNullable()
 
       table.integer('employee_id').unsigned().notNullable()
