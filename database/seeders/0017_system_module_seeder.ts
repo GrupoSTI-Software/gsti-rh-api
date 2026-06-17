9099d285b2d2c61ddb95f4c27da703f37d376af6
wilvardo <wilvardo@gmail.com>
2026-06-17 10:27:43 -0600

    feat: Agregar módulos y funcionalidades para Departamentos y Puestos
    Se han añadido nuevos módulos y funcionalidades en los seeders correspondientes. Los módulos 'Departamentos' y 'Puestos' ahora incluyen sus respectivas características, como alta y edición, baja, y exportación a Excel, entre otras. Esto mejora la estructura del sistema y facilita la gestión de estos elementos en la aplicación.

--- database/seeders/0017_system_module_seeder.ts
+++ database/seeders/0017_system_module_seeder.ts
@@ -38,6 +38,64 @@ export default class extends BaseSeeder {
         </svg>`,
       },
       {
+        systemModuleId: 2,
+        systemModuleName: 'Departamentos',
+        systemModuleSlug: 'departments',
+        systemModuleDescription: 'departments',
+        systemModules: 1,
+        systemModulePath: '/departments',
+        systemModuleGroup: '2. Empresa',
+        systemModuleActive: 1,
+        systemModuleIcon:
+        `<svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="48"
+          height="48"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="M3 21l18 0" />
+          <path d="M9 8l1 0" />
+          <path d="M9 12l1 0" />
+          <path d="M9 16l1 0" />
+          <path d="M14 8l1 0" />
+          <path d="M14 12l1 0" />
+          <path d="M14 16l1 0" />
+          <path d="M5 21v-16a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v16" />
+        </svg>`,
+      },
+      {
+        systemModuleId: 3,
+        systemModuleName: 'Puestos',
+        systemModuleSlug: 'positions',
+        systemModuleDescription: 'positions',
+        systemModules: 1,
+        systemModulePath: '/positions',
+        systemModuleGroup: '2. Empresa',
+        systemModuleActive: 1,
+        systemModuleIcon:
+        `<svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="48"
+          height="48"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="M3 7m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
+          <path d="M8 7v-2a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v2" />
+          <path d="M12 12l0 .01" />
+          <path d="M3 13a20 20 0 0 0 18 0" />
+        </svg>`,
+      },
+      {
         systemModuleId: 4,
         systemModuleName: 'Periodos Vacacionales',
         systemModuleSlug: 'vacations',
