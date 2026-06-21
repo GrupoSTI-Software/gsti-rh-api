7a9930ddd528f707eddcff4186a269655b1a4ed2
Fernando Canales <fernando.cnls@outlook.com>
2026-06-21 16:51:37 -0600

    feat: Agregar nuevas claves y ruta para exámenes de eventos traumáticos
    Se han añadido nuevas claves para los exámenes de eventos traumáticos en el formateador de respuestas estándar. Además, se ha importado la ruta correspondiente para gestionar los exámenes de eventos traumáticos en la aplicación.
    
    Esto mejora la funcionalidad relacionada con los eventos traumáticos y su manejo en la aplicación.

--- app/helpers/standard_response_formatter.ts
+++ app/helpers/standard_response_formatter.ts
@@ -104,6 +104,8 @@ export class StandardResponseFormatter {
       'Traumatic Event Report': 'traumaticEventReport',
       'Traumatic Event Referrals': 'traumaticEventReferrals',
       'Traumatic Event Referral': 'traumaticEventReferral',
+      'Traumatic Event Exams': 'traumaticEventExams',
+      'Traumatic Event Exam': 'traumaticEventExam',
       'Repse': 'repseRegistrations',
       'Repse Registrations': 'repseRegistrations',
       'Repse Registration': 'repseRegistration',
